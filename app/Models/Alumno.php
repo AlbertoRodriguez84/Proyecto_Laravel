2efc23523f3566b278a878e668171dc0355c1a57
Alberto Rodriguez <arodriguezpal@gmail.com>
2024-05-19 12:25:44 +0200

    Profesores creados en BD

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\ProfesorController;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Alumno extends Model
 {
     use HasFactory;
+
     protected $fillable = ['nombre', 'DNI', 'edad', 'email'];
+
+    public function profesor()
+    {
+        return $this->belongsTo(Profesor::class);
+    }
 }
